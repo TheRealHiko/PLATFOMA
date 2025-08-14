d18cf6680a2d5e6c61a180068c37b5ceef8782e1
TheRealHiko <ayaanbi4147m@gmail.com>
2025-08-14 17:37:01 +0530

    lvl selector

--- scripts/world_icon.gd
+++ scripts/world_icon.gd
@@ -2,11 +2,60 @@
 extends Control
 
 @export var world_index: int = 1
+@onready var player_icon = $PlayerIcon
 
-func _ready() -> void:
-	$Label.text = " World " +str(world_index)
+# Each world has an icon node and a linked scene file
+@onready var levels = [
+	{
+		"node": $WorldIconEasy,
+		"scene": "res://scenes/game.tscn"
+	},
+	{
+		"node": $WorldIconNormal,
+		"scene": "res://scenes/game2.tscn"
+	},
+	{
+		"node": $WorldIconInsane,
+		"scene": "res://scenes/game3.tscn"
+	},
+	{
+		"node": $WorldIconMedium,
+		"scene": "res://scenes/game4.tscn"
+	},
+	{
+		"node": $WorldIconImpossible,
+		"scene": "res://scenes/game5.tscn"
+	},
+	{
+		"node": $WorldIconHard,
+		"scene": "res://scenes/game6.tscn"
+	}
+]
 
+var current_level: int = 0
 
-func _process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		$Label.text = " World " +str(world_index)
+func _ready():
+	# Move player icon to first world
+	_update_player_position()
+
+func _input(event):
+	# Move left
+	if event.is_action_pressed("vi_left") and current_level > 0:
+		current_level -= 1
+		_update_player_position()
+
+	# Move right
+	if event.is_action_pressed("vi_right") and current_level < levels.size() - 1:
+		current_level += 1
+		_update_player_position()
+
+	# Confirm selection
+	if event.is_action_pressed("vi_accept"):
+		var scene_path = levels[current_level]["scene"]
+		get_tree().change_scene_to_file(scene_path)
+
+func _update_player_position():
+	if player_icon and levels[current_level]["node"]:
+		player_icon.global_position = levels[current_level]["node"].global_position
+	else:
+		push_warning("PlayerIcon or level node not found!")
---
+++ scripts/world_select.gd
@@ -0,0 +1,35 @@
+extends Control
+
+# Which world index to start on
+@export var world_index: int = 0
+
+# Store world icons and scene paths together
+@onready var levels = [
+	{ "node": $WorldIconEasy, "scene": "res://scenes/game.tscn" },
+	{ "node": $WorldIconNormal, "scene": "res://scenes/game2.tscn" },
+	{ "node": $WorldIconInsane, "scene": "res://scenes/game3.tscn" },
+	{ "node": $WorldIconMedium, "scene": "res://scenes/game4.tscn" },
+	{ "node": $WorldIconImpossible, "scene": "res://scenes/game5.tscn" },
+	{ "node": $WorldIconHard, "scene": "res://scenes/game6.tscn" }
+]
+
+var current_level: int = 0
+
+func _ready():
+	# Place player icon at the first world's position
+	_update_player_position()
+
+func _input(event):
+	if event.is_action_pressed("vi_left") and current_level > 0:
+		current_level -= 1
+		_update_player_position()
+
+	if event.is_action_pressed("vi_right") and current_level < levels.size() - 1:
+		current_level += 1
+		_update_player_position()
+
+	if event.is_action_pressed("vi_accept"):
+		get_tree().change_scene_to_file(levels[current_level]["scene"])
+
+func _update_player_position():
+	$PlayerIcon.global_position = levels[current_level]["node"].global_position
