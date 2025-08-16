7cdde33df752fa402df7dbb96ea51ed2ecfd47b8
TheRealHiko <ayaanbi4147m@gmail.com>
2025-08-16 15:30:10 +0530

    game 3

--- scripts/world_select.gd
+++ scripts/world_select.gd
@@ -6,7 +6,7 @@ extends Control
 # Store world icons and scene paths together
 @onready var levels = [
 	{ "node": $WorldIconEasy, "scene": "res://scenes/game2.tscn" },
-	{ "node": $WorldIconNormal, "scene": "res://scenes/game3.tscn" },
+	{ "node": $WorldIconNormal, "scene": "res://scenes/game_3.tscn" },
 	{ "node": $WorldIconInsane, "scene": "res://scenes/game.tscn" },
 	{ "node": $WorldIconMedium, "scene": "res://scenes/game4.tscn" },
 	{ "node": $WorldIconImpossible, "scene": "res://scenes/game5.tscn" },
