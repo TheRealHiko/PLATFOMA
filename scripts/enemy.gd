a066953b0f51139f27c920febe8160ccc05e6932
TheRealHiko <ayaanbi4147m@gmail.com>
2025-08-19 15:20:39 +0530

    android exported and coal added

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,7 +6,8 @@ var direction = 1
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 60
+const SPEED = 150
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if ray_cast_right.is_colliding():
