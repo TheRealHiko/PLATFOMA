fbb86d80c44c38b66ff09447b03fbb5471da652a
TheRealHiko <ayaanbi4147m@gmail.com>
2025-08-21 17:23:38 +0530

    game 5 part 5

--- scripts/exit.gd
+++ scripts/exit.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var timer_panel: Panel = $Player/Camera2D/CanvasLayer/Panel
 
 func _input(event):
-	if event.is_action_pressed("exit"):
+	if event.is_action_pressed("exit") and Engine.time_scale > 0.9:
 		# First reset stored timer value
 		if timer_panel:
 			timer_panel.reset_saved_time_but_keep_display()
